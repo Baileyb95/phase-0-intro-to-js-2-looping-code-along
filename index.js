43897b4af53d5e36e85cf3c0c093b372137d6e86
Bailey Bell Loffredo <baileybell.loffredo@gmail.com>
2023-07-16 21:11:33 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(nameArray, birthday){
+   let empty = []
+for (let i=0; i < nameArray.length; i++) {
+          empty.push(`Thank you, ${nameArray[i]}, for the wonderful ${birthday} gift!`);
+
+        }
+      
+        return empty;
+      }
+function countDown(){
+    let countDown = 0;
+while (countDown < 11) {
+  console.log(countDown++);
+}
+}
